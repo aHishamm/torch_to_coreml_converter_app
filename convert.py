7e6fbb9baf9493bdf919590fdd53403f4d80c7e8
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2024-02-13 17:21:04 +0400

    Updated convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,17 @@
 import torch 
 import torchvision 
 import coremltools as ct 
-
+import argparse 
 #Loading the EfficientSAM model
-#Will Add the code later 
+#Path to vits 
+#/Users/ahishamm/Documents/projects/efficientSAM/EfficientSAM/torchscripted_model/efficient_sam_vits_torchscript.pt
+#Path to vitt 
+#/Users/ahishamm/Documents/projects/efficientSAM/EfficientSAM/torchscripted_model/efficient_sam_vitt_torchscript.pt
+
+parser = argparse.ArgumentParser(description='Add the path to the Pytorch model: ')
+parser.add_argument('--path',type=str,help='Specify the path to the Pytorch model',required=True) 
+args = parser.parse_args() 
+print(f'Path to Pytorch model: {args.path}')
+#Loading the model
+model = torch.jit.load(args.path) 
+model.eval() 
